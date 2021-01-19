f01d29d14d27900dc11d4263e07c7d0728312953
giangkhuat <khuatgia@grinnell.edu>
2021-01-19 15:31:30 -0600

    Add recipient schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const {Schema} = mongoose.Schema;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
     title : String,
     body: String,
     subject: String,
-    // recipients is a array of strings
-    recipients: [String]
+    yes: {type: Number, default: 0},
+    no: {type: Number, default: 0},
+    recipients: [RecipientSchema]
 });
 
 mongoose.model('surveys', surveySchema);
